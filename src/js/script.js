1c97f61b44a6623bb68fce86866df82df4a86f2e
Zaznwa <talipovazamat2@gmail.com>
2021-12-01 17:29:36 +0600

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: true,
+
+
+    });
+});
+
+
+
+
